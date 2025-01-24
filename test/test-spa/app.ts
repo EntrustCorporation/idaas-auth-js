a12dedf0f985363362112c3c5fd7c7d9027e0bea
Andrew MacCuaig <Andrew.MacCuaig@entrust.com>
2025-01-24 15:27:07 -0500

    fix test app

--- test/test-spa/app.ts
+++ test/test-spa/app.ts
@@ -1,17 +1,46 @@
-import testSpa from "./index.html";
+import { join } from "node:path";
+import { $ } from "bun";
 
-const staticServer = Bun.serve({
-  port: 8080,
-  static: {
-    "/": testSpa,
-  },
-  async fetch(req) {
-    const url = new URL(req.url);
+const baseDir = import.meta.dir;
+const outDir = join(baseDir, "dist");
 
-    console.info(req.method, url.pathname);
+console.log(process.env.CLIENT_ID);
 
-    return new Response();
-  },
-});
+(async () => {
+  console.log("♻️ Cleaning", outDir);
 
-console.log("🚀", staticServer.url.origin);
+  await $`rm -rf ${outDir}`;
+
+  console.log("🏗️ Building", baseDir);
+
+  await Bun.build({
+    entrypoints: [join(baseDir, "./index.ts")],
+    outdir: outDir,
+    root: baseDir,
+    env: "inline",
+  });
+
+  console.log("🍦 Serving", baseDir);
+
+  const server = Bun.serve({
+    port: 8080,
+    development: true,
+    async fetch(req) {
+      const url = new URL(req.url);
+
+      const filename = url.pathname === "/" ? "index.html" : url.pathname;
+
+      console.info("🟢", req.method, filename);
+
+      const fullPath = join(baseDir, filename);
+
+      const bunfile = Bun.file(fullPath);
+
+      await bunfile.stat();
+
+      return new Response(bunfile);
+    },
+  });
+
+  console.log("🚀", server.url.origin);
+})();
